2153b2a688710d889f5866207a925dad0699f2aa
Filip Kis <filip.kis@gmail.com>
2014-02-25 16:22:41 +0100

    fixed string data flavor

--- src/se/kth/csc/iprog/draw/swing/ShapeTransferable.java
+++ src/se/kth/csc/iprog/draw/swing/ShapeTransferable.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,95 +17,119 @@ import se.kth.csc.iprog.draw.model.Shape;
 import se.kth.csc.iprog.draw.model.ShapeContainer;
 
 /**
- * A Transferable representation of shapes. Reads and writes shapes to files, and allows them to be transfered within a
- * Swing application or between applications. This class does not depend on any view (canvas, form...) hence it is
- * placed in this package. Serializing shapes into string, maybe in SVG format, could actually abstracted up to the
- * model.
+ * A Transferable representation of shapes. Reads and writes shapes to files,
+ * and allows them to be transfered within a Swing application or between
+ * applications. This class does not depend on any view (canvas, form...) hence
+ * it is placed in this package. Serializing shapes into string, maybe in SVG
+ * format, could actually abstracted up to the model.
  * 
  * @author cristi
  */
 public class ShapeTransferable implements Transferable {
 
-    /**
-     * the shape to be transferred. TODO: more shapes could be transferred at a time
-     */
-    Shape shape;
+	/**
+	 * the shape to be transferred. TODO: more shapes could be transferred at a
+	 * time
+	 */
+	Shape shape;
 
-    /**
-     * The list of files representing the transferred shapes
-     */
-    List<File> shapeFileList;
+	/**
+	 * The list of files representing the transferred shapes
+	 */
+	List<File> shapeFileList;
 
-    // could be multiple shapes
-    public ShapeTransferable(Shape s) {
-        this.shape = s;
-    }
+	// could be multiple shapes
+	public ShapeTransferable(Shape s) {
+		this.shape = s;
+	}
 
-    /**
-     * For now only files are supported. However, we could also support strings, in order to transfer within a swing
-     * application without creating a file
-     */
-    static DataFlavor[] supported = { DataFlavor.javaFileListFlavor, DataFlavor.plainTextFlavor };
+	/**
+	 * For now only files are supported. However, we could also support strings,
+	 * in order to transfer within a swing application without creating a file
+	 */
+	static DataFlavor[] supported = { DataFlavor.javaFileListFlavor,
+			DataFlavor.stringFlavor };
 
-    @Override
-    public DataFlavor[] getTransferDataFlavors() {
-        return supported;
-    }
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return supported;
+	}
 
-    @Override
-    public boolean isDataFlavorSupported(DataFlavor flavor) {
-        return Arrays.asList(supported).contains(flavor);
-    }
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		return Arrays.asList(supported).contains(flavor);
+	}
 
-    /**
-     * write shape to a temporary file and return a list of only one file
-     */
-    @Override
-    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
-        if (!isDataFlavorSupported(flavor))
-            throw new UnsupportedFlavorException(flavor);
-        // if we have already made the file list, we just return it
-        if (flavor.equals(DataFlavor.plainTextFlavor)) {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            shape.writeTo(bos);
-            bos.close();
-            return new ByteArrayInputStream(bos.toByteArray());
-        } else {
-            if (shapeFileList != null)
-                return shapeFileList;
+	/**
+	 * write shape to a temporary file and return a list of only one file
+	 */
+	@Override
+	public Object getTransferData(DataFlavor flavor)
+			throws UnsupportedFlavorException, IOException {
+		if (!isDataFlavorSupported(flavor))
+			throw new UnsupportedFlavorException(flavor);
+		// if we have already made the file list, we just return it
+		if (flavor.equals(DataFlavor.stringFlavor)) {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			shape.writeTo(bos);
+			bos.close();
+			return bos.toString("UTF-8");
+		} else {
+			if (shapeFileList != null)
+				return shapeFileList;
 
-            // otherwise we make a file list
-            File shapeFile = File.createTempFile("/tmp/" + shape.getType(), ".properties");
-            FileOutputStream out = new FileOutputStream(shapeFile);
-            shape.writeTo(out);
-            out.close();
-            return Arrays.asList(shapeFile);
-        }
-    }
+			// otherwise we make a file list
+			File shapeFile = File
+					.createTempFile(shape.getType(), ".properties");
+			FileOutputStream out = new FileOutputStream(shapeFile);
+			shape.writeTo(out);
+			out.close();
+			return Arrays.asList(shapeFile);
+		}
+	}
 
-    /**
-     * Read shapes from files and add them to a model. Utility method for entities that receive a list of shape files
-     * 
-     * @param transferable
-     * @param model
-     * @param index
-     */
-    public static boolean addToModel(Transferable transferable, ShapeContainer model, int index) {
-        // get the transferred data
-        try {
-            @SuppressWarnings("unchecked")
-            List<File> transferData = (List<File>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
+	/**
+	 * Read shapes from files and add them to a model. Utility method for
+	 * entities that receive a list of shape files
+	 * 
+	 * @param transferable
+	 * @param model
+	 * @param index
+	 */
+	public static boolean addToModel(Transferable transferable,
+			ShapeContainer model, int index) {
+		// get the transferred data
+		InputStream in;
+		try {
+			@SuppressWarnings("unchecked")
+			List<File> transferData = (List<File>) transferable
+					.getTransferData(DataFlavor.javaFileListFlavor);
 
-            for (File f : transferData) {
-                FileInputStream in = new FileInputStream(f);
-                model.addShape(Shape.readFrom(in), index++);
-                in.close();
+			for (File f : transferData) {
+				in = new FileInputStream(f);
+				model.addShape(Shape.readFrom(in), index++);
+				in.close();
 
-            }
-            return true;
-        } catch (Throwable t) {
-            t.printStackTrace();
-            return false;
-        }
-    }
+			}
+			return true;
+		} catch (UnsupportedFlavorException ufe) {
+			try {
+				String shape = (String) transferable
+						.getTransferData(DataFlavor.stringFlavor);
+				in = new ByteArrayInputStream(shape.getBytes("UTF-8"));
+				model.addShape(Shape.readFrom(in), index++);
+				in.close();
+				return true;
+			} catch (UnsupportedFlavorException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		return false;
+	}
 }
--- src/se/kth/csc/iprog/draw/swing/form/ShapeTransferListController.java
+++ src/se/kth/csc/iprog/draw/swing/form/ShapeTransferListController.java
@@ -18,108 +18,115 @@ import se.kth.csc.iprog.draw.swing.ShapeTransferable;
  * @author cristi
  */
 final class ShapeTransferListController extends TransferHandler {
-    private final JList<Shape> list;
+	private final JList<Shape> list;
 
-    private final ShapeContainer model;
+	private final ShapeContainer model;
 
-    /**
-     * Enable drag and drop / copy-paste on the list
+	/**
+	 * Enable drag and drop / copy-paste on the list
+	 * 
+	 * @param list
+	 * @param model
+	 */
+	ShapeTransferListController(JList<Shape> list, ShapeContainer model) {
+		this.list = list;
+		this.model = model;
+		list.setDragEnabled(true);
+		list.setDropMode(DropMode.INSERT);
+		list.setTransferHandler(this);
+	}
+
+	// ---------------- SOURCE methods
+	/**
+	 * data transfer semantic
+	 */
+	@Override
+	public int getSourceActions(JComponent source) {
+		System.out.println("** getSourceActions call");
+		return TransferHandler.COPY_OR_MOVE;
+	}
+
+	Shape dragged;
+
+	/**
+	 * put the shape into a tranferrable form
+	 */
+	@Override
+	public Transferable createTransferable(JComponent source) {
+		System.out.println("** createTransferable call");
+		// TODO: there may be more shapes selected
+		dragged = list.getSelectedValue();
+		return new ShapeTransferable(dragged);
+	}
+
+	/**
+	 * remove the dragged element once it was transferred somewhere else
+	 */
+	@Override
+	protected void exportDone(JComponent source, Transferable data, int action) {
+		System.out.println("** exportDone call");
+
+		if (source != list)
+			return;
+
+		if (action == TransferHandler.MOVE) {
+			model.removeShape(dragged);
+		}
+
+	}
+
+	// ---------------- TARGET methods
+	/**
+	 * called during drag over, to see whether we can accept a drop
+	 */
+	@Override
+	public boolean canImport(TransferHandler.TransferSupport support) {
+		System.out.println("canImport call");
+
+		// we simply check whether the data transferred can come as a file list
+		// (among other flavors)
+
+		// we could actually obtain the file list
+		// like this:
+		// support.getTransferable().getTransferData(DataFlavor.javaFileListFlavor)
+		// and check to see whether it actually contains shape files
+		// but then again we have to take into account that this method is
+		// called very often
+
+		return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor)
+				|| support.isDataFlavorSupported(DataFlavor.stringFlavor);
+	}
+
+	/**
+	 * Drop or paste took place
+	 */
+	@Override
+	public boolean importData(TransferHandler.TransferSupport support) {
+		// we cannot assume that canImport was called, e.g. it is not called for
+		// copy-paste
+		if (!canImport(support))
+			return false;
+
+		System.out.println("** importData call");
+
+		// where will the new shape land? The default is
+		// "above the currently selected"
+		int addIndex = list.getSelectedIndex();
+
+		// drag and drop provides a precise location
+		if (support.isDrop())
+			addIndex = ((JList.DropLocation) support.getDropLocation())
+					.getIndex();
+
+		// add the transferred data to the model
+		return ShapeTransferable.addToModel(support.getTransferable(), model,
+				addIndex);
+
+	}
+
+	/**
      * 
-     * @param list
-     * @param model
      */
-    ShapeTransferListController(JList<Shape> list, ShapeContainer model) {
-        this.list = list;
-        this.model = model;
-        list.setDragEnabled(true);
-        list.setDropMode(DropMode.INSERT);
-        list.setTransferHandler(this);
-    }
-
-    // ---------------- SOURCE methods
-    /**
-     * data transfer semantic
-     */
-    @Override
-    public int getSourceActions(JComponent source) {
-        System.out.println("** getSourceActions call");
-        return TransferHandler.COPY_OR_MOVE;
-    }
-
-    Shape dragged;
-
-    /**
-     * put the shape into a tranferrable form
-     */
-    @Override
-    public Transferable createTransferable(JComponent source) {
-        System.out.println("** createTransferable call");
-        // TODO: there may be more shapes selected
-        dragged = list.getSelectedValue();
-        return new ShapeTransferable(dragged);
-    }
-
-    /**
-     * remove the dragged element once it was transferred somewhere else
-     */
-    @Override
-    protected void exportDone(JComponent source, Transferable data, int action) {
-        System.out.println("** exportDone call");
-
-        if (source != list)
-            return;
-
-        if (action == TransferHandler.MOVE) {
-            model.removeShape(dragged);
-        }
-
-    }
-
-    // ---------------- TARGET methods
-    /**
-     * called during drag over, to see whether we can accept a drop
-     */
-    @Override
-    public boolean canImport(TransferHandler.TransferSupport support) {
-        System.out.println("canImport call");
-
-        // we simply check whether the data transferred can come as a file list (among other flavors)
-
-        // we could actually obtain the file list
-        // like this: support.getTransferable().getTransferData(DataFlavor.javaFileListFlavor)
-        // and check to see whether it actually contains shape files
-        // but then again we have to take into account that this method is called very often
-
-        return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor)
-                || support.isDataFlavorSupported(DataFlavor.plainTextFlavor);
-    }
-
-    /**
-     * Drop or paste took place
-     */
-    @Override
-    public boolean importData(TransferHandler.TransferSupport support) {
-        // we cannot assume that canImport was called, e.g. it is not called for copy-paste
-        if (!canImport(support))
-            return false;
-
-        System.out.println("** importData call");
-
-        // where will the new shape land? The default is "above the currently selected"
-        int addIndex = list.getSelectedIndex();
-
-        // drag and drop provides a precise location
-        if (support.isDrop())
-            addIndex = ((JList.DropLocation) support.getDropLocation()).getIndex();
-
-        // add the transferred data to the model
-        return ShapeTransferable.addToModel(support.getTransferable(), model, addIndex);
-
-    }
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 }
